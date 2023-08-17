e57591b8943d1039414ed628a445349d1f377683
matheuscri <80854803+matheuscri@users.noreply.github.com>
2023-08-17 13:22:41 -0300

    Add files via upload

--- Classforpeople.py
+++ Classforpeople.py
@@ -11,13 +11,18 @@ class People:
 
     def Changemetods(self):
         
-        print("What's the new age?")
-        self=input(self.age)
-        print("What's the new home?")
-        self=input(self.home)
+        self.age= "50"
+        self.home= "Paris"
+    
+    def Callresults(self):
+
+        print("Person name is"+ self.personname +" the age is: " +self.age + " born in " + self.bornlocation + " the home now is: " + self.home)
 
 
  # for exemple I will create a a name
 
 p1=People("Laura","28","Belfort","Toulouse")
-print(p1)
+p1.Callresults()
+p1.Changemetods()
+p1.Callresults()
+
